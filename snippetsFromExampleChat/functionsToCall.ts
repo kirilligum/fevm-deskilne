b5e83a44a06f74c0368596187f9a6004ae4870fc
stan-dot <stanislawmalinowski09@gmail.com>
2022-11-20 09:33:53 +0000

    moved deps to dev

---
+++ snippetsFromExampleChat/functionsToCall.ts
@@ -0,0 +1,10 @@
+
+ function listenCost(address listener){}
+
+    function listenCostSet(address listener, uint amount){}
+
+    function introduceRequest(address initiator, address through, addres to, uint amountToPayForIntro) { }
+
+    function forwardIntro(address initiator, address through, addres to) { }
+
+    function acceptIntro(address initiator, address through, addres to, uint amountStakedOnNewConnection) { }
